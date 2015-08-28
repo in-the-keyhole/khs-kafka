4e9df6b889a17cd9715068546bd83a1316a17163
Joseph Post <jp@jsphpst.com>
2015-08-28 14:53:28 -0500

    update scripst

--- kafka.sh
+++ kafka.sh
@@ -1,5 +1,5 @@
-
+#!/bin/sh
 echo Starting Kafka
  
-chmod a+x kafka_2.10-0.8.2.0/bin/kafka-server-start.sh
-sh kafka_2.10-0.8.2.0/bin/kafka-server-start.sh kafka_2.10-0.8.2.0/config/server.properties
+#chmod a+x kafka_2.10-0.8.2.0/bin/kafka-server-start.sh
+sh kafka_2.10-0.8.2.0/bin/kafka-server-start.sh ./kafka_2.10-0.8.2.0/config/server.properties
--- zookeeper.sh
+++ zookeeper.sh
@@ -1,5 +1,5 @@
-
+#!/bin/sh
 echo Starting ZooKeeper
  
-chmod a+x kafka_2.10-0.8.2.0/bin/zookeeper-server-start.sh
+#chmod a+x kafka_2.10-0.8.2.0/bin/zookeeper-server-start.sh
 sh kafka_2.10-0.8.2.0/bin/zookeeper-server-start.sh kafka_2.10-0.8.2.0/config/zookeeper.properties
