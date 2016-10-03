4f3b86533675317db979b230a9714ffc4f0d585f
Matt M. <matt.g.mccandless@gmail.com>
2016-10-03 16:23:28 -0500

    Window bat files for running in a Windows environment
    Make sure JAVA_HOME doesn't have any spaces in it and is a JDK.

---
+++ kafka.bat
@@ -0,0 +1,3 @@
+ECHO Starting Kafka
+
+kafka_2.10-0.8.2.0/bin/windows/kafka-server-start.bat kafka_2.10-0.8.2.0/config/server.properties
---
+++ zookeeper.bat
@@ -0,0 +1,3 @@
+ECHO Starting Zookeeper
+
+kafka_2.10-0.8.2.0/bin/windows/zookeeper-server-start.bat kafka_2.10-0.8.2.0/config/zookeeper.properties
